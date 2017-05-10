91dca7fdf358daf1a408c6c10933332852370ff2
Beianca <beiancayousif89@gmail.com>
2017-05-10 13:39:55 +0000

    add fields to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  
+  protected
+     def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :first_name, :last_name, :username, :phone, :birthday, :gender) }
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit({ roles: [] }, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :first_name, :last_name, :username,:phone, :birthday, :gender) }
+     end
 end
